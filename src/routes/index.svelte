32689984e683aacc2ba16961638a6b56b17909f8
Marcelo Henrique <mhnegrao@gmail.com>
2021-04-02 20:45:28 -0300

    Preparando ambiente

---
+++ src/routes/index.svelte
@@ -0,0 +1,50 @@
+<script>
+	import successkid from "images/successkid.jpg";
+</script>
+
+<svelte:head>
+	<title>Cozinha da Adri</title>
+</svelte:head>
+
+<h1>Cozinha da Adri!</h1>
+
+<figure>
+	<img alt="Success Kid" src={successkid} />
+	<figcaption>Have fun with Sapper!</figcaption>
+</figure>
+
+<style>
+	h1,
+	figure,
+	p {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
